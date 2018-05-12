84c014e857fae2864ff32bbb357e6794cc6cfb83
CarmeloRicarte <carmeloricarterocamora@gmail.com>
2018-05-12 13:26:45 +0200

    Cambio de atributo del método veredicto y añadido javadoc sobre este método

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,13 @@ public class Main {
         System.out.println("La calificación de " + args[0] + ":" + args[1]);
         System.out.println(veredicto(args[1],args[2]));
     }
+
+    /**
+     * compara dos valores y devuelve un mensaje
+     * @param valor1 nota que creo que voy a sacar
+     * @param valor2 nota real del curso
+     * @return mensaje que devuelve según la comparación de ambas notas
+     */
     private static String veredicto(String valor1, String valor2){
         if(valor1.equals(valor2)){
             return "Perfecto. Has tenido buen criterio.";
@@ -14,5 +21,5 @@ public class Main {
         if(Integer.valueOf(valor1) < Integer.valueOf(valor2)){
             return "Has mejorado, o te ha llegado el éxito inesperado.";
         }
-        return "Te has confiado. Falta realismo.";    }
+        return "Te has confiado. Falta realismo.";}
 }
